726b69a4632bbb12cb4f2ace471ff8d551be3ed4
Lindsay loughlin <loughlil@loughlils-MacBook-Pro.local>
2021-11-29 19:08:43 +1100

    add in demo mod use

---
+++ go.mod
@@ -0,0 +1,5 @@
+module my.example.project.com
+
+go 1.15
+
+require github.com/lindsayloughlin/demomodule v0.1.5
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/lindsayloughlin/demomodule/common"
+)
+
+func main() {
+	data := common.NewData()
+	fmt.Printf("%s is amazing", data)
+}
